c335ace8255ac4ce9039d19393111263ac8044e3
sumtyme-team <ade.jinadu@sumtyme.ai>
2025-06-29 10:14:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,18 +15,18 @@ with col2:
     st.image('sumtyme_logo.png')
 # --- End of the code to center the image ---
 
-st.markdown("""
+st.write("""
 With our platform, developers and enterprises can model dynamic systems instantly, saving time and costly training runs. Our proprietary technology fundamentally changes how intelligent AI systems are built, creating models that can reason, adapt, and think beyond their training data.""")
 
 tab1, tab2 = st.tabs(["Key Benefits", "Video Demo"])
 
 with tab1:
 
-    st.markdown("""
+    st.write("""
     Key Benefits:
     """)
     
-    st.markdown("""
+    st.write("""
     - **Zero Training Data Dependence:** Our platform provides the core intelligence needed to model dynamic systems, removing the traditional reliance on training data.
     - **Zero Feature Engineering & Fine-Tuning:** We eliminate the complex and time-consuming process of manually creating features or adapting existing models, streamlining model development.
     - **Zero Retraining Cycles:** Our solution eliminates frequent, resource-intensive retraining, ensuring continuous model effectiveness without the need for constant updates.
@@ -34,8 +34,8 @@ with tab1:
 
 with tab2:
 
-    st.markdown("""Demonstrating our ability to model noisy, high-frequency data without training or retraining, our API was used to build a four-timeframe network (1, 2, 3, and 4-second intervals) to model the market dynamics of the SPY ETF.""")
-    st.markdown("""The technology successfully anticipated a market decline 10 minutes before Trump's tariff announcement on April 2nd, 2025.""")
+    st.write("""Demonstrating our ability to model noisy, high-frequency data without training or retraining, our API was used to build a four-timeframe network (1, 2, 3, and 4-second intervals) to model the market dynamics of the SPY ETF.""")
+    st.write("""The technology successfully anticipated a market decline 10 minutes before Trump's tariff announcement on April 2nd, 2025.""")
     
     video_bytes = "https://github.com/sumtyme-ade/live-demo-1/releases/download/v1.0.0/20252905.mp4"
 
