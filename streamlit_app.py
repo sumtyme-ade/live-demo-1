2838c8bb27e6f41d4befaa5a47df0b3bf831968b
sumtyme-team <ade.jinadu@sumtyme.ai>
2025-06-29 11:03:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,7 @@ with tab2:
     st.write("""Demonstrating our ability to model noisy, high-frequency data without training or retraining, our API was used to build a four-timeframe network (1, 2, 3, and 4-second intervals) to model the market dynamics of the SPY ETF.""")
     st.write("""The technology successfully anticipated a market decline 10 minutes before Trump's tariff announcement on April 2nd, 2025.""")
     
-    video_bytes = "https://github.com/sumtyme-ade/live-demo-1/releases/download/v1.0.0/20252905.mp4"
+    video_file = open("20252905.mp4", "rb")
+    video_bytes = video_file.read()
 
     st.video(video_bytes)
